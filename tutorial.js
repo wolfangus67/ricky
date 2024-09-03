97b8c8b08002d0e3dd098e869bc70213f9b1ac32
wolfangus67 <gilles.braconnier@gmail.com>
2024-09-03 07:30:33 +0200

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,5 +1,9 @@
+// tutorial.js
+
 export function openYoutubeViewer(songName) {
     const searchQuery = encodeURIComponent(`ricky somborn tutorial ${songName}`);
     const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${searchQuery}`;
+    
+    // Ouvrir l'URL dans un nouvel onglet
     window.open(youtubeSearchUrl, '_blank');
 }
