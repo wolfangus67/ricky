c01716fdf2240f8106e82ddd396863913f12b778
wolfangus67 <gilles.braconnier@gmail.com>
2024-09-03 05:32:32 +0200

    Update translations.js

--- translations.js
+++ translations.js
@@ -3,6 +3,9 @@ const translations = {
         'title': 'Tablatures Ukulélé',
         'viewPdf': 'Voir la tablature PDF',
         'viewTutorial': 'Voir le tuto',
+        'playAudio': 'Lecture',
+        'searchPlaceholder': 'Rechercher une chanson ou un artiste',
+        'searchButton': 'Rechercher',
         'prevPage': 'Page précédente',
         'nextPage': 'Page suivante',
         'close': 'Fermer',
@@ -12,6 +15,9 @@ const translations = {
         'title': 'Ukulele Tabs',
         'viewPdf': 'View PDF Tab',
         'viewTutorial': 'View Tutorial',
+        'playAudio': 'Play',
+        'searchPlaceholder': 'Search for a song or artist',
+        'searchButton': 'Search',
         'prevPage': 'Previous Page',
         'nextPage': 'Next Page',
         'close': 'Close',
@@ -21,11 +27,12 @@ const translations = {
         'title': 'Tablaturas de Ukulele',
         'viewPdf': 'Ver tablatura PDF',
         'viewTutorial': 'Ver tutorial',
+        'playAudio': 'Reproducir',
+        'searchPlaceholder': 'Buscar una canción o artista',
+        'searchButton': 'Buscar',
         'prevPage': 'Página anterior',
         'nextPage': 'Página siguiente',
         'close': 'Cerrar',
         'pageOf': 'Página {current} de {total}'
     }
 };
-
-export default translations;
