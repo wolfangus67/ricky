4968230da9e55425a3a57eb0429abc3c2571360d
wolfangus67 <gilles.braconnier@gmail.com>
2024-09-03 04:13:21 +0200

    Create translation.js

---
+++ translation.js
@@ -0,0 +1,31 @@
+const translations = {
+    'fr': {
+        'title': 'Tablatures Ukulélé',
+        'viewPdf': 'Voir la tablature PDF',
+        'viewTutorial': 'Voir le tuto',
+        'prevPage': 'Page précédente',
+        'nextPage': 'Page suivante',
+        'close': 'Fermer',
+        'pageOf': 'Page {current} sur {total}'
+    },
+    'en': {
+        'title': 'Ukulele Tabs',
+        'viewPdf': 'View PDF Tab',
+        'viewTutorial': 'View Tutorial',
+        'prevPage': 'Previous Page',
+        'nextPage': 'Next Page',
+        'close': 'Close',
+        'pageOf': 'Page {current} of {total}'
+    },
+    'es': {
+        'title': 'Tablaturas de Ukulele',
+        'viewPdf': 'Ver tablatura PDF',
+        'viewTutorial': 'Ver tutorial',
+        'prevPage': 'Página anterior',
+        'nextPage': 'Página siguiente',
+        'close': 'Cerrar',
+        'pageOf': 'Página {current} de {total}'
+    }
+};
+
+export default translations;
