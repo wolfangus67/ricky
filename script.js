414dcf36ce0df4992507b81f0fcac3a8a3cccd4c
wolfangus67 <gilles.braconnier@gmail.com>
2024-09-03 03:08:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ async function getSongList() {
             const songName = file.name.replace('.pdf', '').replace(/_/g, ' ');
             const songElement = document.createElement('section');
             songElement.className = 'song';
-            // Utilisez l'URL de visualisation
-            const fileUrl = `https://raw.githubusercontent.com/wolfangus67/ricky/main/songs/${file.name}`;
+            // Utilisez l'URL brute de GitHub
+            const fileUrl = `https://github.com/wolfangus67/ricky/raw/main/songs/${file.name}`;
             songElement.innerHTML = `
                 <h2>${songName}</h2>
                 <a href="${fileUrl}" target="_blank">Voir la tablature PDF</a>
